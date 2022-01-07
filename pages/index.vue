699df75cd487c6239e201694d608a4f8f79b76b8
Jerell Kuo <jkfox67@gmail.com>
2022-01-07 20:48:56 +0800

    axios basic example

--- pages/index.vue
+++ pages/index.vue
@@ -30,6 +30,7 @@ export default {
   },
   methods: {
     async fetchAPI () {
+      /* axios GET method 寫法之一 */
       const _options = {
         method: 'GET',
         url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=5&offset=0'
@@ -37,6 +38,26 @@ export default {
       const _result = await this.$axios.$request(_options)
       console.log(_result.data)
       this.data = _result.data
+
+      /* axios GET method 寫法之二 */
+      // const _url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=5&offset=0'
+      // const _result = await this.$axios.$get(_url)
+
+      /* axios POST method 寫法之一 */
+      // const _options = {
+      //   method: 'POST',
+      //   url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=5&offset=0',
+      //   headers : {
+      //    xxxxxxx: xxxxxxxxxx
+      //   }
+      // }
+      // const _result = await this.$axios.$request(_options)
+
+      /* axios POST method 寫法之二 */
+      // const _url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=5&offset=0'
+      // const _pkg = { xxxx: xxxx }
+      // const _config = { headers: xxxxxxxxxxxxx }
+      // const _result = await this.$axios.$get(_url , _pkg, config)
     },
     showDetail (e) {
       console.log(e)
